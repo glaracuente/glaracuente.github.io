868beb670da7af99bb943d62176fb5f6341f82ce
glaracuente <glaracuente@outbrain.com>
2018-11-03 11:26:03 -0400

    responsiveness

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,9 @@
 var activePage = "Home";
+var mQ = window.matchMedia("(max-width: 768px)")
 
 $(document).on("click", ".nav-item", function () {
     activePage = $(this).text()
-   
+
     $(".nav-item").css({ "color": "#585858", "font-weight": "unset" });
     $(this).css({ "color": "#e1e1e1", "font-weight": "unset" })
 
@@ -13,8 +14,31 @@ $(".nav-item").hover(function () {
 }, function () {
     $(this).css({ "color": "#585858", "font-weight": "unset" });
 
-    if ($(this).text()=== activePage) {
+    if ($(this).text() === activePage) {
         $(".nav-item").css({ "color": "#585858", "font-weight": "unset" });
         $(this).css({ "color": "#e1e1e1", "font-weight": "unset" })
     }
 });
+
+$(".nav-bar").hover(function () {
+    if (mQ.matches) { // If media query matches
+        console.log("hey")
+        $("ul").css('display', 'none')
+    }
+}, function () {
+    $("ul").css('display', 'inline-block')
+});
+
+
+
+
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction)
+
+function myFunction(x) {
+    if (x.matches) { // If media query matches
+        document.body.style.backgroundColor = "yellow";
+    } else {
+        document.body.style.backgroundColor = "pink";
+    }
+}
