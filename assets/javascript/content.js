85ee6b873d48707219d07213f14afc0f65807cb5
glaracuente <glaracuente@outbrain.com>
2018-11-04 23:00:23 -0500

    updates

--- assets/javascript/content.js
+++ assets/javascript/content.js
@@ -1,41 +1,45 @@
-var mobile = false;
-
-function checkSize() {
-    if ($("#nav-bar")[0].clientHeight === 60) {
-        mobile = true;
-    }
-    else {
-        mobile = false;
-    }
-}
+var mobile;
+var winWidth;
+var adjustedWidth
 
 
-//dynamically set content width on window resize
-$(window).resize(function () {
-    checkSize()
+$(document).ready(function () {
+    //check window size and dynamically set content margins
+    function adjustMargins() {
+        if ($("#nav-bar")[0].clientHeight === 60) {
+            mobile = true;
+        }
+        else {
+            mobile = false;
+        }
 
-    if (mobile) {
-        var winWidth = $(window).width();
-        $("#content").css('width', '100%');
-        $("#contact-container").css('margin-left', (winWidth - 200) / 2);
+        if (mobile) {
+            winWidth = $(window).width();
+            $("#content").css('width', '100%');
+            $("#contact-container").css('margin-left', (winWidth - 200) / 2);
+            $("#blog-container").css('margin-left', (winWidth - 200) / 2);
+            $("#home-container").css('margin-left', (winWidth - 400) / 2);
+            $("#about-container").css('margin-left', (winWidth - 400) / 2);
+            $("#resume-container").css('margin-left', (winWidth - 400) / 2);
+            $("#profile-con").css("display", "none");
+        }
+        else {
+            adjustedWidth = ($(window).width() - 200);
+            $("#content").css('width', adjustedWidth);
+            $("#contact-container").css('margin-left', (adjustedWidth - 420) / 2);
+            $("#blog-container").css('margin-left', (adjustedWidth - 200) / 2);
+            $("#home-container").css('margin-left', (adjustedWidth - 400) / 2);
+            $("#about-container").css('margin-left', (adjustedWidth - 500) / 2);
+            $("#resume-container").css('margin-left', (adjustedWidth - 700) / 2);
+            $("#profile-con").css("display", "inline-block");
+        }
     }
-    else {
-        var newWidth = ($(window).width() - 200);
-        $("#content").css('width', newWidth);
-        $("#contact-container").css('margin-left', (newWidth - 420) / 2);
-    }
+
+
+    //dynamically set content width on window resize
+    $(window).resize(function () {
+        adjustMargins()
+    });
+    //dynamically set content width on page refresh
+    adjustMargins()
 });
-
-
-checkSize()
-
-//dynamically set content width on page refresh
-if (mobile) {
-    var winWidth = $(window).width();
-    $("#contact-container").css('margin-left', (winWidth - 200) / 2);
-}
-else {
-    var newWidth = ($(window).width() - 200);
-    $("#content").css('width', newWidth);
-    $("#contact-container").css('margin-left', (newWidth - 420) / 2);
-}
--- assets/javascript/nav.js
+++ assets/javascript/nav.js
@@ -4,50 +4,52 @@
 // 17181B is background color - can use to make text invisible 
 
 var activePage = "home";
-var pages = ["home", "about me", "resume", "portfolio", "blog", "contact"]
+var pages = ["home", "about", "resume", "portfolio", "blog", "contact"]
 
-$(".nav-item").on("click", function () {
-    activePage = $(this).data("content")
+$(document).ready(function () {
+    $(".nav-item").on("click", function () {
+        activePage = $(this).data("content")
 
-    //Makes only clicked nav item highlighted 
-    $(".nav-item").css('color', '#585858');
-    $(this).css('color', '#e1e1e1');
+        //Makes only clicked nav item highlighted 
+        $(".nav-item").css('color', '#585858');
+        $(this).css('color', '#e1e1e1');
 
-    //Makes content area show desired nav item content
+        //Makes content area show desired nav item content
+        for (var i = 0; i < pages.length; i++) {
+            $("#" + pages[i]).css('display', 'none');
+        }
+        $("#" + activePage).css('display', 'inline-block');
+    });
+
+    $(".nav-item").hover(function () {
+        // Sharpens color of only hovered item and makes it bold
+        $(this).css({ "color": "#979797", "font-weight": "bold" });
+    }, function () {
+        $(this).css({ "color": "#585858", "font-weight": "unset" });
+
+        if ($(this).data("content") === activePage) {
+            $(this).css('color', '#e1e1e1');
+        }
+    });
+
+    $("#bitmoji-pic").on("click", function () {
+        $("#profile-con").css({"display": "inline-block","float": "left", "height": "200px", "width": "200px"});  
+    });
+
+    $("#profile-con").on("click", function () {
+        if (mobile) {
+        $("#profile-con").css("display", "none");  
+        }
+    });
+
+
+
+    //On page refresh, set home as the active page
+    $("#home-nav-item").css('color', '#e1e1e1');
+
     for (var i = 0; i < pages.length; i++) {
         $("#" + pages[i]).css('display', 'none');
     }
-    $("#" + activePage).css('display', 'inline-block');
+
+    $("#home").css('display', 'inline-block');
 });
-
-$(".nav-item").hover(function () {
-    // Sharpens color of only hovered item and makes it bold
-    $(this).css({ "color": "#979797", "font-weight": "bold" });
-}, function () {
-    $(this).css({ "color": "#585858", "font-weight": "unset" });
-
-    if ($(this).data("content") === activePage) {
-        $(this).css('color', '#e1e1e1');
-    }
-});
-
-/*$("#nav-bar").hover(function () {
-    if ($("#nav-bar")[0].clientHeight === 200) {
-        $("ul").css('display', 'inline-block');
-        $("#mobile-nav-temp").css('display', 'none');
-    }
-}, function () {
-    if ($("#nav-bar")[0].clientHeight <= 200) {
-        $("ul").css('display', 'none');
-        $("#mobile-nav-temp").css('display', 'inline-block');
-    }
-});*/
-
-
-//On page refresh, set home as the active page
-$("#home-nav-item").css('color', '#e1e1e1');
-
-for (var i = 0; i < pages.length; i++) {
-    $("#" + pages[i]).css('display', 'none');
-}
-$("#home").css('display', 'inline-block');
